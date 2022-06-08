d827c8cc545cf576347f071d0f8a8ffb17ac3caa
Jason Shands <jason.shands@gmail.com>
2022-06-07 21:45:51 -0500

    Added implementation to query cosmosdb

--- HuntsvilleFood.Functions.Js/GetRestaurantHealthRatings/index.js
+++ HuntsvilleFood.Functions.Js/GetRestaurantHealthRatings/index.js
@@ -1,13 +1,44 @@
+const CosmosClient = require("@azure/cosmos").CosmosClient;
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const search = (req.query.search || (req.body && req.body.search));
+    if (search == null) {
+        context.res = {
+            status: 400,
+            body: 'Missing required search parameter',
+        }
+
+        return;
+    }
+
+    if (search.length < 3) {
+        context.res = {
+            status: 400,
+            body: 'search parameter must be at least 3 characters',
+        }
+
+        return;
+    }
+
+    const client = new CosmosClient({
+        endpoint: process.env["COSMOSDB_ENDPOINT"],
+        key: process.env["COSMOSDB_KEY"]
+    });
+
+    const database = client.database("HuntsvilleFood");
+    const container = database.container("Ratings");
+
+
+    const { resources: items } = await container.items
+        .query({
+            query: "SELECT * from c where LOWER(c.establishment) like \"%" + search.toLowerCase() + "%\""
+        })
+        .fetchAll();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: items
     };
 }
